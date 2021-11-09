95b06f28f9787a10fd5257cbcdfe9e43574e91a0
Mr_B <l27001@ezdomain.ru>
2021-11-09 19:56:10 +0300

    Дамп БД

--- shawel.sql
+++ shawel.sql
@@ -1,103 +1,88 @@
--- MariaDB dump 10.19  Distrib 10.6.4-MariaDB, for Linux (aarch64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: **HOST**    Database: shawel_new
--- ------------------------------------------------------
--- Server version	10.6.4-MariaDB-log
+-- Host: **HOST**:3306
+-- Generation Time: Nov 09, 2021 at 04:55 PM
+-- Server version: 10.6.4-MariaDB-log
+-- PHP Version: 7.4.21-dev
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `shawel_new`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `chats`
 --
 
-DROP TABLE IF EXISTS `chats`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `chats` (
   `id` int(11) NOT NULL,
-  `subscribe` int(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  KEY `subscribe` (`subscribe`)
+  `subscribe` int(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `chats`
---
-
-LOCK TABLES `chats` WRITE;
-/*!40000 ALTER TABLE `chats` DISABLE KEYS */;
-/*!40000 ALTER TABLE `chats` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `vkid` int(30) NOT NULL,
   `dostup` int(11) NOT NULL DEFAULT 0,
-  `subscribe` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`vkid`),
-  KEY `subscribe` (`subscribe`)
+  `subscribe` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `vk`
 --
 
-DROP TABLE IF EXISTS `vk`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vk` (
-  `rasp` varchar(200) NOT NULL,
-  `zvonki` varchar(200) NOT NULL,
-  `rasp-updated` varchar(64) NOT NULL,
-  `rasp-checked` varchar(64) NOT NULL,
   `rasp_last` varchar(256) NOT NULL,
-  `zvonki_last` varchar(256) NOT NULL
+  `zvonki_last` varchar(256) NOT NULL,
+  `rasp_post` varchar(64) NOT NULL,
+  `zvonki_post` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `vk`
 --
 
-LOCK TABLES `vk` WRITE;
-/*!40000 ALTER TABLE `vk` DISABLE KEYS */;
-INSERT INTO `vk` VALUES ('', '', '', '', '', '');
-/*!40000 ALTER TABLE `vk` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `vk` (`rasp_last`, `zvonki_last`, `rasp_post`, `zvonki_post`) VALUES
+('', '', '', '');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subscribe` (`subscribe`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`vkid`),
+  ADD KEY `subscribe` (`subscribe`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-10-11 19:59:01
